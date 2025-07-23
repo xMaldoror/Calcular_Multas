d6284719555689bca83618341bb7b49ea7dfcfa8
Francisco Pereira <francisco.pereira993@gmail.com>
2025-07-23 13:28:44 +0100

    Arranjo De Codigo
    A estrutura de codigo não me deixava satisfeito. Isto porque separava a programação de interação com o utilizador, intercalando as funções. Dessa forma, foi atribuido dorectamente o conceito de "velocidade" na defenição de cada função.

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-#Front_end_1.1
-print("Bem vindo ao Calculador de Multas\n")
-
-#Conceitos operativos 
-#Front_end_1.2
-while True:    
-    try:
-        velocidade = float(input("A que velocidade circulava: "))
-        break
-    except ValueError:
-        print("Erro: a velocidade deve ser um número inteiro positivo.")
-        
-#Funções
-def localidade():
+#Funções    
+def localidade(velocidade):
     if velocidade > 50:
         print("\n-->A sua multa será de 60€<--")
     elif velocidade >= 90:
@@ -19,13 +7,13 @@ def localidade():
     elif velocidade >= 120:
         print("\n-->A sua  multa será de 320€<--")
     
-def fora_da_localidade():
+def fora_da_localidade(velocidade):
     if velocidade > 90:
         print("\n-->A sua multa será de 60€<--")
     elif velocidade >= 120:
         print("\n-->A sua  multa será de 120€<--")
 
-def autoestrada():
+def autoestrada(velocidade):
     if velocidade > 120:
         print("\n-->A sua multa será de 60€<--")
     elif velocidade >= 150:
@@ -33,8 +21,16 @@ def autoestrada():
     elif velocidade >= 175:
         print("\n-->A sua  multa será de 360€<--")
     
+#Front_end
+print("Bem vindo ao Calculador de Multas\n")
 
-#Front_end_2
+while True:    
+    try:
+        velocidade = float(input("A que velocidade circulava: "))
+        break
+    except ValueError:
+        print("Erro: a velocidade deve ser um número inteiro positivo.")
+        
 while True:
     print("\nOnde circulava o veiculo?")
     print("Escolha uma opção:")
@@ -47,13 +43,13 @@ while True:
     
 
     if loc == 1:
-        localidade()
+        localidade(velocidade)
     elif loc == 2:
-        fora_da_localidade()
+        fora_da_localidade(velocidade)
     elif loc == 3:
-        autoestrada()
+        autoestrada(velocidade)
     elif loc == 0:
-        print("Calcular multas encerrado!")
+        print("Calcular Multas encerrado!")
         break
     else:
         print("Erro! Introduza um numero de 1 a 3") 
