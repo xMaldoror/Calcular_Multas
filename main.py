2bced624c8a0ac5adb5228ddb0a6d08d952681c2
Francisco Pereira <francisco.pereira993@gmail.com>
2025-07-23 13:07:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
+#Intro
+print("Bem vindo ao Calculador de Multas\n")
+
+#Conceitos operativos
+while True:    
+    try:
+        velocidade = float(input("A que velocidade circulava: "))
+        break
+    except ValueError:
+        print("Erro: a velocidade deve ser um número inteiro positivo.")
+        
+#Funções
 def localidade():
+    if velocidade > 50:
+        print("\n-->A sua multa será de 60€<--")
+    elif velocidade >= 90:
+        print("\n-->A sua multa será de 120€<--")
+    elif velocidade >= 120:
+        print("\n-->A sua  multa será de 320€<--")
     
 def fora_da_localidade():
-    
+    if velocidade > 90:
+        print("\n-->A sua multa será de 60€<--")
+    elif velocidade >= 120:
+        print("\n-->A sua  multa será de 120€<--")
+
 def autoestrada():
-    
+    if velocidade > 120:
+        print("\n-->A sua multa será de 60€<--")
+    elif velocidade >= 150:
+        print("\n-->A sua multa será de 120€<--")
+    elif velocidade >= 175:
+        print("\n-->A sua  multa será de 360€<--")
     
 
-
+#Front_end
 while True:
-    print("Onde circulava o veiculo?")
+    print("\nOnde circulava o veiculo?")
     print("Escolha uma opção:")
     print("1 - Localidade")
     print("2 - Fora da localidade")
     print("3 - Autoestrada")
     print("0 - Sair")
     
-    loc = int(input("Introduza o local: "))
+    loc = int(input("\nIntroduza o local: "))
+    
 
-    if loc == "1":
+    if loc == 1:
         localidade()
-    elif loc == "2":
+    elif loc == 2:
         fora_da_localidade()
-    elif loc == "3":
+    elif loc == 3:
         autoestrada()
-    elif loc == "0":
+    elif loc == 0:
         print("Calcular multas encerrado!")
         break
     else:
